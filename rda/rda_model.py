4c4e2d95d232748c8ef826b26e209415f4282d78
Jihoon Kim <jihoonerd@outlook.com>
2021-11-04 09:46:08 +0900

    Update RDA model

--- rda/rda_model.py
+++ rda/rda_model.py
@@ -12,9 +12,7 @@ class RDA(BaseEstimator):
     def __init__(self, alpha=0.0, beta=0.0, variance=0):
         """
         alpha, beta, and varaince decide the characteristics of RDA.
-
         Followings are remarks of RDA upon settings of hyperparameters:
-
         alpha = beta = 0:  QDA
         alpha=0, beta=1: LDA
         alpha=1, beta=0: nearest mean classifier
@@ -27,28 +25,26 @@ class RDA(BaseEstimator):
         self.variance = variance
 
         self.class_labels = []
-        self.class_priors = {}
-        self.class_means = {}
+        self.priors = {}
+        self.means = {}
 
         self.rda_cov = {}
         
     def fit(self, X, y):
-
         self.class_labels = np.unique(y)
-
         S_i = {}
         pooled_covariance = 0
-        # Calculate class priors, class means, and LDA pooled covariance matrix
+
         for i in self.class_labels:
             indices = np.where(y == i)[0]
             samples = X[indices, :]
 
-            self.class_priors[i] = float(len(indices)) / len(y) # Class ratio
-            self.class_means[i] = np.mean(samples, axis=0) # Mean vector of each features (N,NUM_FEATURES)
+            self.priors[i] = float(len(indices)) / len(y) # class ratio (prior)
+            self.means[i] = np.mean(samples, axis=0) # mean vector of each features (N,NUM_FEATURES)
 
             S_i[i] = np.cov(samples, rowvar=0)
 
-            pooled_covariance += S_i[i] * self.class_priors[i] # Equation (3) above.
+            pooled_covariance += S_i[i] * self.priors[i] # equation (3) above.
 
         # Calculate RDA regularized covariance matricies for each class
         for i in self.class_labels:
@@ -59,28 +55,22 @@ class RDA(BaseEstimator):
 
         if not self.fitted:
             raise NameError('Fit model first')
-        # Determine probability of each class given input vector
 
         predictions = []
 
         for i in range(len(X)):
-
             x = X[i]
             discriminant_score = {}
             for i in self.class_labels:
-                # Divid the class delta calculation into 3 parts
                 part1 = -0.5 * np.linalg.det(self.rda_cov[i])
-                part2 = -0.5 * (x - self.class_means[i]).T @ np.linalg.pinv(self.rda_cov[i]) @ (x - self.class_means[i])
-                part3 = np.log(self.class_priors[i])
-                discriminant_score[i] = part1 + part2 + part3 # 5.19
-            
+                part2 = -0.5 * (x - self.means[i]).T @ np.linalg.pinv(self.rda_cov[i]) @ (x - self.means[i])
+                part3 = np.log(self.priors[i])
+                discriminant_score[i] = part1 + part2 + part3 # 5.19 in textbook
             prediction = max(discriminant_score, key=discriminant_score.get)
             predictions.append(prediction)
-
         return predictions
 
     def score(self, X, y_true):
-
         y_pred = self.predict(X)
         f1_macro = f1_score(y_true, y_pred, average='macro')
         return f1_macro
